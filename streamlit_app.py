c38bd2af6a64a5f0c53587e720e372b42440db39
benagastov <106045877+benagastov@users.noreply.github.com>
2022-11-28 15:12:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,19 @@ import streamlit as st
 
 # importing the library
 import numpy as np
-import matplotlib.pyplot as plt
- 
+import pandas as pd
+
 # data to be plotted
 rentangmax = st.slider('Berapa rentang max yang kamu inginkan?', 0, 130, 25)
 st.write("rentang max nya sebesar ", rentangmax, 'satuan')
 x = np.arange(1, rentangmax)
 y = np.array([100, 10, 300, 20, 500, 60, 700, 80, 900, 100])
  
-# plotting
-plt.title("Line graph")
-plt.xlabel("X axis")
-plt.ylabel("Y axis")
-plt.plot(x, y, color ="green")
-plt.show()
 
-plt.subplots()
-st.pyplot(plt.subplots())
+
+chart_data = pd.DataFrame(
+    np.array([100, 10, 300, 20, 500, 60, 700, 80, 900, 100]),
+    columns=['a'])
+
+st.line_chart(chart_data)
 
