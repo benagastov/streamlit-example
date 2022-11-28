a2fad0e3c68cf30b08effa4a9c34f0febadd3803
benagastov <106045877+benagastov@users.noreply.github.com>
2022-11-28 14:59:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+# importing the library
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# data to be plotted
+rentangmax = st.slider('Berapa rentang max yang kamu inginkan?', 0, 130, 25)
+st.write("rentang max nya sebesar ", rentangmax, 'satuan')
+x = np.arange(1, rentangmax)
+y = np.array([100, 10, 300, 20, 500, 60, 700, 80, 900, 100])
+ 
+# plotting
+plt.title("Line graph")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.plot(x, y, color ="green")
+plt.show()
+
+
