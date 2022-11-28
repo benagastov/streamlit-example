f333d85b9132dbdf20424c34e8de821a8cb53c79
benagastov <106045877+benagastov@users.noreply.github.com>
2022-11-28 15:18:08 +0700

    Update and rename slidersimpel to slidersimpel.py

---
+++ slidersimpel.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+import streamlit as st
+
+# data to be plotted
+rentangmax = st.slider('Berapa rentang max yang kamu inginkan?', 0, 130, 25)
+st.write("rentang max nya sebesar ", rentangmax, 'satuan')
+
+#create your figure and get the figure object returned
+fig = plt.figure() 
+plt.plot(np.arange(0.0, rentangmax, 0.1)) 
+
+st.pyplot(fig)
+
