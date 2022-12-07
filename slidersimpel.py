1c580624b9355f8bc9ecd3317b4e53920c6d508a
benagastov <106045877+benagastov@users.noreply.github.com>
2022-12-07 21:40:33 +0700

    Update slidersimpel.py

--- slidersimpel.py
+++ slidersimpel.py
@@ -15,6 +15,7 @@ with tab1:
   with col2:
     image = Image.open('Mamako_hugging2.png')
     st.image(image, caption='Google Ads')
+    image = Image.open('Konosuba_Darkness.png')
   with col1:
     # Plot between -10 and 10 with .001 steps.
     x_axis = np.arange(-8, 8, 0.01)
