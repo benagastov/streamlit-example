4e5d7b45d794d9677f65fe41819213d4a1e8b665
benagastov <106045877+benagastov@users.noreply.github.com>
2022-12-08 23:32:57 +0700

    Update slidersimpel.py

--- slidersimpel.py
+++ slidersimpel.py
@@ -9,35 +9,13 @@ import statistics
 
 from streamlit.components.v1 import html
 
-def nav_page(page_name, timeout_secs=3):
-    nav_script = """
-        <script type="text/javascript">
-            function attempt_nav_page(page_name, start_time, timeout_secs) {
-                var links = window.parent.document.getElementsByTagName("a");
-                for (var i = 0; i < links.length; i++) {
-                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
-                        links[i].click();
-                        return;
-                    }
-                }
-                var elasped = new Date() - start_time;
-                if (elasped < timeout_secs * 1000) {
-                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
-                } else {
-                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
-                }
-            }
-            window.addEventListener("load", function() {
-                attempt_nav_page("%s", new Date(), %d);
-            });
-        </script>
-    """ % (page_name, timeout_secs)
-    html(nav_script)
+import streamlit as st
+def redirect(_url):
+link = ‘’
+st.markdown(link, unsafe_allow_html=True)
 
-if st.button("< Prev"):
-    nav_page("Foo")
-if st.button("Next >"):
-    nav_page("google.com")
+if st.button(“Login”):
+redirect(‘http://localhost:8501/Login 12’)
 
 tab1, tab2 = st.tabs(["📈 Grafik Awal", "🗃 Grafik Kustom"])
 with tab1:
